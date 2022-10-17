bd21ec6700a57a3c9db0c7fcd537e99d637dc506
imran-rassi <imran.rassi.2020@gmail.com>
2022-10-17 16:16:48 -0400

    added changes to script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,11 @@
+
+
 window.onload = function () {
 
     // DISPLAY CURRENT WEATHER
+
     // This .on("click") function will trigger the AJAX Call
+    
     $("#search-button").on("click", function (event) {
           event.preventDefault();
           saveToLocalStorage();
@@ -12,32 +16,38 @@ window.onload = function () {
           saveToLocalStorage4();
           searchHistory();
 
+
           // grab text from the search-input box
           var city = $("#search-input").val();
 
-          // construct URL
+          // construct the URL by city
           var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=000401db107b5cbe165fdf198e9f1e47";
 
-          // hit the queryURL with $ajax
-          //take response data and display it in the city-box within the weather-div
+
+          //display the responce data in the city-box within the weather-div
           $.ajax({
                 url: queryURL,
                 method: "GET"
+
                 
             }).then(function (response) {
+
 
                   // Constructing HTML containing the weather information for searched city
                   var cityName = $("<h2>").text(response.name);
                   var cityNameList = $("<li>").text(response.name);
                   cityNameList.addClass("list-group-item");
+                  // console.log()
 
                   var weatherIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
                   var weatherType = $("<p class='bold'>").text(response.weather[0].main);
 
-                  // kelvin to F
+
+
+                  // changing kelvin to Fernhit
                   var tempInt = parseInt(response.main.temp);
                   var tempF = (tempInt * 9 / 5) - 459.67;
-                  var cityTemp = $("<p class='temp'>").text(Math.floor(tempF) + "°");
+                  //console.log()                  var cityTemp = $("<p class='temp'>").text(Math.floor(tempF) + "°");
                   var cityHumidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
                   var cityWindSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
 
@@ -45,10 +55,11 @@ window.onload = function () {
                   $("#city-box").empty();
                   $("#city-box").append(cityName, weatherIcon, weatherType, cityTemp, cityHumidity, cityWindSpeed);
 
+
                   // prepend the searched city onto city-list
                   $("#list-group").prepend(cityNameList);
 
-                  // UV INDEX
+                  // The index for the UV
                   var lat = response.coord.lat;
                   var lon = response.coord.lon;
 
@@ -65,7 +76,7 @@ window.onload = function () {
                         $("#city-box").append("<div id='uv-box' class=''></div>");
                         $("#uv-box").append(uvIndex);
 
-                        // UV Index Color Change 
+                        // changing the color of UV index 
                         if (Math.floor(response2.value) <= 2) {
                               $("#uv-box").addClass("uvFavorable");
                         } if (Math.floor(response2.value) >= 3 && Math.floor(response2.value) <= 8) {
@@ -81,18 +92,22 @@ window.onload = function () {
       
 
 
-      // DISPLAY FIVE DAY FORECAST
+      // The Five day forecast display 
       $("#search-button").on("click", function (event) {
             event.preventDefault();
+            //console.log(searchHistory)
 
             // grab text from the search-input box
             var city = $("#search-input").val();
 
-            // construct URL
+            // The URL construction
+
             var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=000401db107b5cbe165fdf198e9f1e47";
 
-            // hit the queryURL with $ajax
-            //take response data and display it in the city-box within the weather-div
+            // queryURL with $ajax
+
+            //Display the responce data in the city-box within the weather-div
+
             $.ajax({
                   url: queryURL,
                   method: "GET"
@@ -100,18 +115,21 @@ window.onload = function () {
             }).then(function (response) {
 
 
-                  // Constructing HTML containing the five day forecast for searched city
-                  // day one
+
+                  
+                  // DAY (1)
                   var date0 = response.list[0].dt_txt;
                   var slicedate0 = date0.slice(5, 10);
+                  //console.log()
 
                   var date0 = $("<p>").text(slicedate0);
                   var weatherIcon0 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
                   var weatherType0 = $("<p class='bold'>").text(response.list[0].weather[0].main);
-                  // kelvin to°
+                  // changing kelvin to °
                   var tempHighInt0 = parseInt(response.list[0].main.temp_max);
                   var tempHighF0 = (tempHighInt0 * 9 / 5) - 459.67;
                   var cityHighTemp0 = $("<p class='temp'>").text(Math.floor(tempHighF0) + "°");
+                  //console.log()
 
                   var humidity0 = $("<p>").text("Humidity: " + response.list[0].main.humidity + "%");
 
@@ -119,17 +137,18 @@ window.onload = function () {
                   $("#div0").empty();
                   $("#div0").append(date0, weatherIcon0, weatherType0, cityHighTemp0, humidity0);
 
-                  // day two
+                  // DAY (2)
                   var date1 = response.list[8].dt_txt;
                   var slicedate1 = date1.slice(5, 10);
 
                   var date1 = $("<p>").text(slicedate1);
                   var weatherIcon1 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + ".png");
                   var weatherType1 = $("<p class='bold'>").text(response.list[8].weather[0].main);
-                  // kelvin to°
+                  // changing kelvin to°
                   var tempHighInt1 = parseInt(response.list[8].main.temp_max);
                   var tempHighF1 = (tempHighInt1 * 9 / 5) - 459.67;
                   var cityHighTemp1 = $("<p class='temp'>").text(Math.floor(tempHighF1) + "°");
+                  // console.log()
 
                   var humidity1 = $("<p>").text("Humidity: " + response.list[8].main.humidity + "%");
 
@@ -139,14 +158,14 @@ window.onload = function () {
 
                   
 
-                  // day three
+                  // DAY (3)
                   var date2 = response.list[16].dt_txt;
                   var slicedate2 = date2.slice(5, 10);
 
                   var date2 = $("<p>").text(slicedate2);
                   var weatherIcon2 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + ".png");
                   var weatherType2 = $("<p class='bold'>").text(response.list[16].weather[0].main);
-                  // kelvin to°
+                  // changing kelvin to°
                   var tempHighInt2 = parseInt(response.list[16].main.temp_max);
                   var tempHighF2 = (tempHighInt2 * 9 / 5) - 459.67;
                   var cityHighTemp2 = $("<p class='temp'>").text(Math.floor(tempHighF2) + "°");
@@ -157,17 +176,18 @@ window.onload = function () {
                   $("#div2").empty();
                   $("#div2").append(date2, weatherIcon2, weatherType2, cityHighTemp2, humidity2);
 
-                  // day four
+                  // DAY (4)
                   var date3 = response.list[24].dt_txt;
                   var slicedate3 = date3.slice(5, 10);
 
                   var date3 = $("<p>").text(slicedate3);
                   var weatherIcon3 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.list[24].weather[0].icon + ".png");
                   var weatherType3 = $("<p class='bold'>").text(response.list[24].weather[0].main);
-                  // kelvin to°
+                  // changing kelvin to°
                   var tempHighInt3 = parseInt(response.list[24].main.temp_max);
                   var tempHighF3 = (tempHighInt3 * 9 / 5) - 459.67;
                   var cityHighTemp3 = $("<p class='temp'>").text(Math.floor(tempHighF3) + "°");
+                  //console.log()
 
                   var humidity3 = $("<p>").text("Humidity: " + response.list[24].main.humidity + "%");
 
@@ -175,19 +195,20 @@ window.onload = function () {
                   $("#div3").empty();
                   $("#div3").append(date3, weatherIcon3, weatherType3, cityHighTemp3, humidity3);
 
-                  // day five
+                  // DAY (5)
                   var date4 = response.list[32].dt_txt;
                   var slicedate4 = date4.slice(5, 10);
 
                   var date4 = $("<p>").text(slicedate4);
                   var weatherIcon4 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.list[32].weather[0].icon + ".png");
                   var weatherType4 = $("<p class='bold'>").text(response.list[32].weather[0].main);
-                  // kelvin to°
+                  // changing kelvin to°
                   var tempHighInt4 = parseInt(response.list[32].main.temp_max);
                   var tempHighF4 = (tempHighInt4 * 9 / 5) - 459.67;
                   var cityHighTemp4 = $("<p class='temp'>").text(Math.floor(tempHighF4) + "°");
 
                   var humidity4 = $("<p>").text("Humidity: " + response.list[32].main.humidity + "%");
+
 
                   // Empty the contents of the city-box div, append the current weather of searched city
                   $("#div4").empty();
@@ -198,7 +219,8 @@ window.onload = function () {
       });
 
 }
-// LOCAL STORAGE
+
+// ADDING LOCAL STORAGE
 
 // save to local storage: city-box (current  weather div)
 const storageInput = document.querySelector("#city-box");
@@ -208,6 +230,7 @@ const storedInput = localStorage.getItem('textinput');
 if (storageInput) {
       text.innerHTML = storedInput;
 }
+//console.log()
 
 storageInput.addEventListener('input', letter => {
       text.innerHTML = letter.target.value;
@@ -216,7 +239,7 @@ storageInput.addEventListener('input', letter => {
 const saveToLocalStorage = () => {
       localStorage.setItem('textinput', text.innerHTML);
 }
-// save to local storage: day 1 five-day-box (5 day forecast)
+// save to local storage: DAY (1) 
 const storageInput0 = document.querySelector("#div0");
 const text0 = document.querySelector("#div0");
 const storedInput0 = localStorage.getItem('textinput0');
@@ -233,7 +256,7 @@ const saveToLocalStorage0 = () => {
       localStorage.setItem('textinput0', text0.innerHTML);
 }
 
-// save to local storage: day 2 five-day-box (5 day forecast)
+// save to local storage: DAY (2)
 const storageInput1 = document.querySelector("#div1");
 const text1 = document.querySelector("#div1");
 const storedInput1 = localStorage.getItem('textinput1');
@@ -250,7 +273,7 @@ const saveToLocalStorage1 = () => {
       localStorage.setItem('textinput1', text1.innerHTML);
 }
 
-// save to local storage: day 3 five-day-box (5 day forecast)
+// save to local storage: DAY (3)
 const storageInput2 = document.querySelector("#div2");
 const text2 = document.querySelector("#div2");
 const storedInput2 = localStorage.getItem('textinput2');
@@ -267,7 +290,7 @@ const saveToLocalStorage2 = () => {
       localStorage.setItem('textinput2', text2.innerHTML);
 }
 
-// save to local storage: day 4 five-day-box (5 day forecast)
+// save to local storage: DAY (4)
 const storageInput3 = document.querySelector("#div3");
 const text3 = document.querySelector("#div3");
 const storedInput3 = localStorage.getItem('textinput3');
@@ -284,7 +307,7 @@ const saveToLocalStorage3 = () => {
       localStorage.setItem('textinput3', text3.innerHTML);
 }
 
-// save to local storage: day 5 five-day-box (5 day forecast)
+// save to local storage: DAY (5)
 const storageInput4 = document.querySelector("#div4");
 const text4 = document.querySelector("#div4");
 const storedInput4 = localStorage.getItem('textinput4');
@@ -303,7 +326,9 @@ const saveToLocalStorage4 = () => {
 
 
 // WHEN I click on a city in the search history
+
 //THEN I am again presented with current and future conditions for that city
+
 function searchHistory() {
 
       $("#city-list").on('click', 'li', function () {
@@ -312,10 +337,10 @@ function searchHistory() {
             var city = $(this).text();
 
 
-            // construct URL
+            // construct the URL
             var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=000401db107b5cbe165fdf198e9f1e47";
 
-            // hit the queryURL with $ajax
+            
             //take response data and display it in the city-box within the weather-div
             $.ajax({
                   url: queryURL,
@@ -331,7 +356,7 @@ function searchHistory() {
                   var weatherIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
                   var weatherType = $("<p class='bold'>").text(response.weather[0].main);
 
-                  // kelvin to F
+                  // changing kelvin to F
                   var tempInt = parseInt(response.main.temp);
                   var tempF = (tempInt * 9 / 5) - 459.67;
                   var cityTemp = $("<p class='temp'>").text("Current Temperature: " + Math.floor(tempF) + "°");
@@ -374,6 +399,7 @@ function searchHistory() {
       });
 
             // DISPLAY FIVE DAY FORECAST
+
             $("#city-list").on('click', 'li', function () {
 
                   console.log($(this).text());
@@ -392,7 +418,7 @@ function searchHistory() {
       
       
                         // Constructing HTML containing the five day forecast for searched city
-                        // day one
+                        // DAY (1)
                         var date0 = response.list[0].dt_txt;
                         var slicedate0 = date0.slice(5, 10);
       
@@ -410,7 +436,7 @@ function searchHistory() {
                         $("#div0").empty();
                         $("#div0").append(date0, weatherIcon0, weatherType0, cityHighTemp0, humidity0);
       
-                        // day two
+                        // DAY (2)
                         var date1 = response.list[8].dt_txt;
                         var slicedate1 = date1.slice(5, 10);
       
@@ -429,18 +455,19 @@ function searchHistory() {
                         $("#div1").append(date1, weatherIcon1, weatherType1, cityHighTemp1, humidity1);
       
       
-                        // day three
+                        // DAY (3)
                         var date2 = response.list[16].dt_txt;
                         var slicedate2 = date2.slice(5, 10);
       
                         var date2 = $("<p>").text(slicedate2);
                         var weatherIcon2 = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + ".png");
                         var weatherType2 = $("<p class='bold'>").text(response.list[16].weather[0].main);
-                        // kelvin to°
+                        // changing kelvin to°
                         var tempHighInt2 = parseInt(response.list[16].main.temp_max);
                         var tempHighF2 = (tempHighInt2 * 9 / 5) - 459.67;
                         var cityHighTemp2 = $("<p class='temp'>").text(Math.floor(tempHighF2) + "°");
-      
+                        //console.log()
+
                         var humidity2 = $("<p>").text("Humidity: " + response.list[16].main.humidity + "%");
       
                         // Empty the contents of the city-box div, append the current weather of searched city
@@ -461,8 +488,9 @@ function searchHistory() {
       
                         var humidity3 = $("<p>").text("Humidity: " + response.list[24].main.humidity + "%");
       
-                        // Empty the contents of the city-box div, append the current weather of searched city
+                        //append the current weather of searched city
                         $("#div3").empty();
+
                         $("#div3").append(date3, weatherIcon3, weatherType3, cityHighTemp3, humidity3);
       
                         // day five
